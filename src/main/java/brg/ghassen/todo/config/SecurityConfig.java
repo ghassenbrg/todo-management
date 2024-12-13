56a6c9f79c5e8594f611a735d07bf48c562d86e6
ghassenbrg <ghassen.brg16@gmail.com>
2024-12-13 18:02:03 +0100

    add authenticationManager Bean and comment inMemoryUsers impl

--- src/main/java/brg/ghassen/todo/config/SecurityConfig.java
+++ src/main/java/brg/ghassen/todo/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package brg.ghassen.todo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -43,21 +45,26 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-
-        UserDetails ghassen = User.builder()
-                .username("ghassen")
-                .password(passwordEncoder().encode("ghassen123"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin123"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(ghassen, admin);
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        UserDetails ghassen = User.builder()
+//                .username("ghassen")
+//                .password(passwordEncoder().encode("ghassen123"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin123"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(ghassen, admin);
+//    }
 
 }
